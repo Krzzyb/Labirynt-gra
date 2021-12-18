c7d98fe2de21d7a8950fa0d51af8e47b05d2d643
Krzzyb <zybura.krzys@gmail.com>
2021-12-18 16:49:54 +0100

    Menu visually done

--- Labirynt.cpp
+++ Labirynt.cpp
@@ -3,24 +3,36 @@
 #include <cstdlib>
 #include <conio.h>
 #include <windows.h>
+#include <cwchar>
 using namespace std;
 void kolory(int k) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, k);
 }
-void pokaz(int tablica[20][20]) {
+void textSize (int size){
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = size;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
+void pokaz(int tablica[40][20]) {
     for (int i = 0; i < 20; i++) {
-        for (int j = 0; j < 20; j++) {
+        for (int j = 0; j < 40; j++) {
             cout << " " << tablica[j][i];
         }
         cout << endl;
     }
 }
 
-void prettyShow(int tablica[20][20], int x, int y) {
+void prettyShow(int tablica[40][20], int x, int y) {
 
     for (int i = 0; i < 20; i++) {
-        for (int j = 0; j < 20; j++) {
+        for (int j = 0; j < 40; j++) {
             if (y == i && x == j) {
                 kolory(1);
                 cout << char(1);
@@ -38,23 +50,31 @@ void prettyShow(int tablica[20][20], int x, int y) {
                     kolory(102);
                     cout << char(219);
                 }
-
+                if (tablica[j][i] == 3){
+                    kolory(109);
+                    cout << char(14);
+                }
+                if (tablica[j][i] == 4){
+                    kolory(108);
+                    cout << char(127);
+                }
             }
             
         }
         cout << endl;
     }
+    kolory(0);
 }
 
 
 
-void randPath(int tabela[20][20] = {0}) {
+void randPath(int tabela[40][20] = {0}) {
     srand(time(NULL));
     int I, X = 0, Y = 0;
     tabela[X][Y] = 1;
-    while (tabela[19][19] == 0) {
+    while (tabela[39][19] == 0) {
         I = rand() % 2;
-        if (I == 0 && X + 1 != 20) {
+        if (I == 0 && X + 1 != 40) {
             X++;
             tabela[X][Y] = 1;
         }
@@ -64,15 +84,29 @@ void randPath(int tabela[20][20] = {0}) {
         }
     }
 }
-void randItems(int tabela[20][20]) {
+void randItems(int tabela[40][20], int bombs, int boxes, int hospitals) {
     srand(time(NULL));
-    int X = 0, Y = 0, amount = 80;
+    int X = 0, Y = 0;
 
-    for (int i = 0; i <= amount; i++) {
-        X = rand() % 20;
+    for (int i = 0; i <= bombs; i++) {
+        X = rand() % 40;
         Y = rand() % 20;
         if (tabela[X][Y] == 0) {
             tabela[X][Y] = 2;
+        }
+    }
+    for (int i = 0; i<=boxes; i++){
+        X = rand() % 40;
+        Y = rand() % 20;
+        if (tabela[X][Y] == 0) {
+            tabela[X][Y] = 3;
+        }
+    }
+    for (int i = 0; i<=hospitals; i++){
+        X = rand() % 40;
+        Y = rand() % 20;
+        if (tabela[X][Y] == 0) {
+            tabela[X][Y] = 4;
         }
     }
 }
@@ -83,34 +117,57 @@ void livescounter(int lives){
     }
 }
 
-void przebieg(int tabela[20][20], int lives){
+void menu(){
+    system("CLS");
+    kolory(7);
+    int n = 0;
+    while (true){
+        
+        system("CLS");
+        cout << "____MENU____\n";
+        if (n == 0) cout << " " << char(16) << " ";
+        cout << "Kontynuuj\n";
+        if (n == 1) cout << " " << char(16) << " ";
+        cout << "Zapisz gre\n";
+        if (n == 2) cout << " " << char(16) << " ";
+        cout << "Zaladuj gre\n";
+        if (n == 3) cout << " " << char(16) << " ";
+        cout << "Wyjdz z gry\n";
+        int i = getch();
+        if(i == 0 || i == 224){
+            i = getch();
+            if (i == 72 && n > 0) n--;
+            else if (i == 80 && n <= 3) n++;
+        }
+
+    }
+    
+}
+void przebieg(int tabela[40][20], int lives){
     int x = 0, y = 0;
     livescounter(lives);
     cout << endl;
     prettyShow(tabela,x,y);
      while (lives>0) {
-        int rozmiar = 20;
+        int rozmiary = 20, rozmiarx = 40;
         int i = getch();
+        if (i == 27) menu();
         if (i == 0 || i == 224){
             i = getch();
             if (i == 72) y--;
             else if (i == 80) y++;
             else if (i == 75) x--;
             else if (i == 77) x++;
-
-            // teleport
-            /*y = (y + rozmiar) % rozmiar;
-            x = (x + rozmiar) % rozmiar;*/
-            // ściany
+            
             if (y < 0) y = 0;
-            else if (y >= rozmiar) y = rozmiar - 1;
+            else if (y >= rozmiary) y = rozmiary - 1;
             if (x < 0) x = 0;
-            else if (x >= rozmiar) x = rozmiar - 1;
+            else if (x >= rozmiarx) x = rozmiarx - 1;
             system("CLS");
             if(tabela[x][y] == 2){
                 lives--;
             }
-            if( x == 19 && y == 19){
+            if( x == 39 && y == 19){
                 cout<<"koniec gry";
                 break;
             }
@@ -122,8 +179,9 @@ void przebieg(int tabela[20][20], int lives){
 }
 int main()
 {
-    int tabela[20][20] = { 0 };
+    int tabela[40][20] = { 0 };
+    textSize(30);
     randPath(tabela);
-    randItems(tabela);
-    przebieg(tabela,3);
+    randItems(tabela, 80, 10, 3);
+    przebieg(tabela, 3);
 }
--- Test.cpp
+++ Test.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <windows.h>
-
+#include <cstdlib>
+#include <conio.h>
+#include <cwchar>
 using namespace std;
 
 void kolor(int k){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, k);
 }
+void size(int size){
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = size;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
 int main(){
     for (int i = 0; i<256; i++){
         kolor(i);
         cout << i<< endl;
     }
+    kolor(7);
+    for (int i = 0; i<=256; i++){
+        cout << i << ". " << char(i)<<endl;
+    }
+    while(true){
+        int i = getch();
+        cout << i << endl;
+    }
+    size(30);
+    cout << "Font: Consolas, Size: 24\n";
     return 0;
 }
