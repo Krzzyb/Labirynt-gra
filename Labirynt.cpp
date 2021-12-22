656a488853872b75114983cec6a72b46ff5ee847
Krzzyb <zybura.krzys@gmail.com>
2021-12-22 16:58:21 +0100

    Wszystko działa jak natura chciała

--- Labirynt.cpp
+++ Labirynt.cpp
@@ -33,6 +33,24 @@ void pokaz(int tablica[40][20]) {
         cout << endl;
     }
 }
+void equipment(string wyposazenie){
+    cout << "____Twoje wyposazenie____\n";
+    if(wyposazenie == "nic"){
+        cout << "Jeszcze nic nie masz, wejdz na pole ";
+        kolory(13);
+        cout << char(14); 
+        kolory(7);
+        cout << " ,aby wylosowac element wyposazenia.\n";
+    }
+    if(wyposazenie == "wykrywacz"){
+        cout << " - wykrywacz metalu (dziala na kratke do przodu, do tylu i na boki)\n";
+    }
+    if(wyposazenie == "sonar"){
+        cout << " - wykrywacz metalu (dziala na kratke do przodu, do tylu i na boki\n";
+        cout << " - sonar (dziala jak wykrywacz metalu, ale na wieksza odleglosc\n";
+    }
+    cout << "________________________________________\n";
+}
 void legenda(string typ){
     if(typ == "gra"){
         cout << "Kontrola strzalkami:" << char(16) << char(17) << char(30) << char(31) << endl;
@@ -77,6 +95,10 @@ void prettyShow(int tablica[40][20], int x, int y) {
                     kolory(128);
                     cout << char(158);
                 }
+                if(tablica[j][i] == 6){
+                    kolory(104);
+                    cout << char(14);
+                }
             }
             
         }
@@ -109,23 +131,56 @@ void darkShow(int tablica[40][20], int x, int y, string wyposazenie){
                         kolory(128);
                         cout << char(158);
                     }
+                    if(tablica[j][i] == 6){
+                        kolory(104);
+                        cout << char(14);
+                    }
                 }
                 
             }
             cout << endl;
         }
     }
+    bool check = false;
     if(wyposazenie == "wykrywacz"){
-        if(tablica[y][x--] == 2){tablica[y][x--] = 6;}
-        if(tablica[y--][x] == 2){tablica[y--][x]= 6;}
-        if(tablica[y++][x] == 2){tablica[y++][x] = 6;}
-        if(tablica[y][x++] == 2){tablica[y][x++] = 6;}
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 40; j++) {
                 if(y == i && x == j){
                     kolory(1);
                     cout << char(1);
-                }else{
+                    check = true;
+                }
+                if((y-1 == i && x == j)||(y == i && x-1 == j)||(y+1 == i && x == j)||(y == i && x+1 == j)){
+                    if (tablica[j][i] == 2) {
+                        kolory(100);
+                        cout << char(15);
+                    }
+                    if (tablica[j][i] == 0) {
+                        kolory(102); 
+                        cout << char(219);
+                    }
+                    if (tablica[j][i] == 3){
+                        kolory(109);
+                        cout << char(14);
+                    }
+                    if (tablica[j][i] == 4){
+                        kolory(108);
+                        cout << char(127);
+                    }
+                    if (tablica[j][i] == 5){
+                        kolory(128);
+                        cout << char(158);
+                    }
+                    if (tablica[j][i] == 1){
+                        kolory(102);
+                        cout << char(219);
+                    }
+                    if(tablica[j][i] == 6){
+                        kolory(104);
+                        cout << char(14);
+                    }
+                }
+                else if(check == false){
                     if (tablica[j][i] == 4){
                         kolory(108);
                         cout << char(127);
@@ -143,21 +198,115 @@ void darkShow(int tablica[40][20], int x, int y, string wyposazenie){
                         cout << char(158);
                     }
                     if(tablica[j][i] == 6){
+                        kolory(104);
+                        cout << char(14);
+                    }
+                    
+                }
+               check = false; 
+            }
+            
+            cout << endl;
+        }
+
+    }
+
+    if(wyposazenie == "sonar"){
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 40; j++) {
+                if(y == i && x == j){
+                    kolory(1);
+                    cout << char(1);
+                    check = true;
+                }
+                if((y-1 == i && x == j)||(y-2 == i && x == j)||
+                    (y == i && x-1 == j)||(y == i && x-2 == j)||
+                    (y+1 == i && x == j)||(y+2 == i && x == j)||
+                    (y == i && x+1 == j)||(y == i && x+2 == j)||
+                    (y-1 == i && x-1 == j)||(y+1 == i && x+1 == j)||
+                    (y-1 == i && x+1 == j)||(y+1 == i && x-1 ==j)){
+                    if (tablica[j][i] == 2) {
                         kolory(100);
                         cout << char(15);
                     }
+                    if (tablica[j][i] == 0) {
+                        kolory(102); 
+                        cout << char(219);
+                    }
+                    if (tablica[j][i] == 3){
+                        kolory(109);
+                        cout << char(14);
+                    }
+                    if (tablica[j][i] == 4){
+                        kolory(108);
+                        cout << char(127);
+                    }
+                    if (tablica[j][i] == 5){
+                        kolory(128);
+                        cout << char(158);
+                    }
+                    if (tablica[j][i] == 1){
+                        kolory(102);
+                        cout << char(219);
+                    }
+                    if(tablica[j][i] == 6){
+                        kolory(104);
+                        cout << char(14);
+                    }
                 }
-                
+                else if(check == false){
+                    if (tablica[j][i] == 4){
+                        kolory(108);
+                        cout << char(127);
+                    }
+                    if(tablica[j][i] == 3){
+                        kolory(109);
+                        cout << char(14);
+                    }
+                    if(tablica[j][i] == 0 || tablica[j][i] == 1 || tablica[j][i] == 2){
+                        kolory(108);
+                        cout << " ";
+                    }
+                    if(tablica[j][i] == 5){
+                        kolory(128);
+                        cout << char(158);
+                    }
+                    if(tablica[j][i] == 6){
+                        kolory(104);
+                        cout << char(14);
+                    }
+                    
+                }
+               check = false; 
             }
+            
             cout << endl;
         }
 
     }
     kolory(7);
+    equipment(wyposazenie);
     legenda("gra");
 }
+void randEquip(string &wyposazenie, int luckFactor){
+    srand(time(NULL));
+    int i = 0;
+    if(wyposazenie == "sonar"){}
+    if(wyposazenie == "wykrywacz"){
+        i = rand() %10;
+        if(i < luckFactor){
+            wyposazenie = "sonar";
+        }
+    }
+    if(wyposazenie == "nic"){
+        i = rand() %10;
+        if(i < luckFactor + 2){
+            wyposazenie = "wykrywacz";
+        }
+    }
+}
 
-void loadGame(int tablica[40][20], int &x, int &y, int &lives){
+void loadGame(int tablica[40][20], int &x, int &y, int &lives, int &luckFactor, string &wyposazenie){
     fstream plik;
     string linijka;
     plik.open("C:\\Intel\\Labirynt_gra\\zapis_gry.txt", ios::in);
@@ -170,6 +319,12 @@ void loadGame(int tablica[40][20], int &x, int &y, int &lives){
         x = stoi(linijka);
         getline(plik, linijka);
         y = stoi(linijka);
+        getline(plik, linijka);
+        getline(plik, linijka);
+        luckFactor = stoi(linijka);
+        getline(plik, linijka);
+        getline(plik, linijka);
+        wyposazenie = linijka;
         for (int i = 0; i < 20; i++) {
             getline(plik, linijka);
             for (int j = 0; j < 40; j++) {
@@ -187,9 +342,9 @@ void loadGame(int tablica[40][20], int &x, int &y, int &lives){
         }
         cout << endl;
     }
-    system("pause");
+    system("CLS");
 }
-void saveGame (int tablica[40][20], int x, int y, int lives){
+void saveGame (int tablica[40][20], int x, int y, int lives, int luckFactor, string wyposazenie){
     string nazwa = "C:\\Intel\\Labirynt_gra\\";
     string tmp = "zapis_gry";
     //cin >> tmp; moze gdzies w przyszlosci zostanie dodane menu roznych zapisow gry
@@ -198,6 +353,8 @@ void saveGame (int tablica[40][20], int x, int y, int lives){
     plik.open(tmp.c_str(), ios::out);
     plik << "lives: \n" << lives << endl;
     plik << "player coordinates: \n" << x << endl << y << endl;
+    plik << "luckFactor: \n" << luckFactor << endl;
+    plik << "equipment: \n" << wyposazenie << endl;
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 40; j++) {
             plik << tablica[j][i];
@@ -222,7 +379,7 @@ void randPath(int tabela[40][20]) {
         }
     }
 }
-void newGameOptions(int &bombs, int &boxes, int &hospitals, int &lives){
+void newGameOptions(int &bombs, int &boxes, int &hospitals, int &lives, int &luckFactor){
     bombs = 0;
     boxes = 0;
     hospitals = 0;
@@ -269,18 +426,21 @@ void newGameOptions(int &bombs, int &boxes, int &hospitals, int &lives){
                 boxes = 30;
                 hospitals = 15;
                 lives = 8;
+                luckFactor = 5;
                 return;
             case 1:
                 bombs = 80;
                 boxes = 20;
                 hospitals = 8;
                 lives = 5;
+                luckFactor = 2;
                 return;
             case 2:
                 bombs = 150;
                 boxes = 10;
                 hospitals = 3;
                 lives = 3;
+                luckFactor = 0;
                 return;
             case 3:
                 do{
@@ -301,6 +461,9 @@ void newGameOptions(int &bombs, int &boxes, int &hospitals, int &lives){
                 }while(740 < bombs + boxes + hospitals);
                 cout << "Ile zyc?  -  ";
                 cin >> lives;
+                cout << "Jaki luckFactor?\n" << "(jest to wartosc od 0 do 10, ktora definuje szczescie w losowaniu wyposazenia";
+                cout << " - jesli chcesz miec pewnosc, ze zawsze cos wylosujesz, wpisz wartosc wieksza od 10)";
+                cin >> luckFactor;
                 return;
             }
         }
@@ -355,7 +518,7 @@ void resetArray(int tabela[40][20] = {0}){
         }
     }
 }
-int menu(int tabela [40][20], int x, int y, int lives){
+int menu(int tabela [40][20], int x, int y, int lives, int luckFactor, string wyposazenie){
     system("CLS");
     kolory(7);
     int n = 0;
@@ -403,7 +566,7 @@ int menu(int tabela [40][20], int x, int y, int lives){
                 return 1;
             case 1:
                 system("CLS");
-                saveGame(tabela, x, y, lives);
+                saveGame(tabela, x, y, lives, luckFactor, wyposazenie);
                 cout << "Zapisano";
                 break;
             case 2:
@@ -417,16 +580,16 @@ int menu(int tabela [40][20], int x, int y, int lives){
     
     
 }
-void przebieg(int tabela[40][20], int lives, int x, int y){
+void przebieg(int tabela[40][20], int lives, int x, int y, int luckFactor, string wyposazenie){
     int setlives = lives;
     livescounter(lives);
     cout << endl;
-    darkShow(tabela,x,y,"wykrywacz");
+    darkShow(tabela,x,y,wyposazenie);
      while (lives>0) {
         int rozmiary = 20, rozmiarx = 40;
         int i = getch();
         if (i == 27){
-            int wynikMenu = menu(tabela, x, y, lives);
+            int wynikMenu = menu(tabela, x, y, lives, luckFactor, wyposazenie);
             if(wynikMenu == 0){
                 return;
             }
@@ -448,18 +611,21 @@ void przebieg(int tabela[40][20], int lives, int x, int y){
                 lives--;
             }
             if(tabela[x][y] == 3){
-                
+                randEquip(wyposazenie, luckFactor);
+                tabela[x][y] = 6;
             }
             if(tabela[x][y] == 4){
                 lives = setlives;
             }
             if( x == 39 && y == 19){
-                cout<<"koniec gry";
+                cout<<"Gratulacje! Dotarles do punktu zbiorki - jestes teraz bezpieczny.\n";
+                cout << "Nacisnij ENTER, aby wrocic do main Menu";
+                getchar();
                 break;
             }
             livescounter(lives);
             cout<<endl;
-            darkShow(tabela,x,y,"wykrywacz");
+            darkShow(tabela,x,y,wyposazenie);
         }
     }
 }
@@ -524,7 +690,8 @@ void mainOptions(int size){
 void startMenu(int tabela [40][20] = {0}){
     kolory(7);
     int n = 0, x = 0, y = 0;
-    int bombs, boxes, hospitals, lives, size;
+    int bombs, boxes, hospitals, lives, size, luckFactor;
+    string wyposazenie;
     while (true){
         system("CLS");
         cout << "____main MENU____\n";
@@ -561,18 +728,19 @@ void startMenu(int tabela [40][20] = {0}){
         if(i == 13){
             switch (n){
             case 0:
-                
+                resetArray(tabela);
+                wyposazenie = "nic";
                 randPath(tabela);
-                newGameOptions(bombs, boxes, hospitals, lives);
+                newGameOptions(bombs, boxes, hospitals, lives, luckFactor);
                 system("CLS");
                 randItems(tabela, bombs, boxes, hospitals);
-                przebieg(tabela, lives, x, y);
+                przebieg(tabela, lives, x, y, luckFactor, wyposazenie);
                 resetArray(tabela);
                 break; 
             case 1:
                 system("CLS");
-                loadGame(tabela, x, y, lives);
-                przebieg(tabela, lives, x, y);
+                loadGame(tabela, x, y, lives, luckFactor, wyposazenie);
+                przebieg(tabela, lives, x, y, luckFactor, wyposazenie);
                 break;
             case 2:
                 mainOptions(size);
